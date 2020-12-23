1130c4534c1c651e9f7ce3aad277ded7f9fe1f6c
Hendrik Schuster <h.schuster@deviantdev.com>
2020-12-23 21:38:42 +0100

    Install and use Material UI for React

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,22 +1,10 @@
-import React from "react";
+import { Container } from '@material-ui/core';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="App">
+    </Container>
   );
 }
 
