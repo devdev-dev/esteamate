ce8f505ba64bf3b048fe3deb8d9bb6f61bbbaf42
Hendrik Schuster <h.schuster@deviantdev.com>
2020-12-24 13:31:34 +0100

    Add a first login screen without any functionality

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,13 @@
 import { Container } from '@material-ui/core';
 import React from 'react';
 import BottomAppBar from './components/BottomAppBar';
+import LoginScreen from './screens/SignInScreen';
 
 function App() {
   return (
     <Container>
       <BottomAppBar />
+      <LoginScreen />
     </Container>
   );
 }
