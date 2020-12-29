5db645d09a051d7553975a0b746c8e2a5cb030af
Hendrik Schuster <h.schuster@deviantdev.com>
2020-12-29 23:27:33 +0100

    Minimal change to test vercel deployment

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
 
       <main className="main">
         <h1 className="title">
-          Learn <a href="https://nextjs.org">Next.js!</a>
+          Getting started with <a href="https://nextjs.org">Next.js!</a>
         </h1>
       </main>
 
