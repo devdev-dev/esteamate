e1f97c1c1bae31646dfecdb50c0900ae7ef032ef
Hendrik Schuster <h.schuster@deviantdev.com>
2020-12-29 10:36:19 +0100

    Use style jsx directly inside the component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,93 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="container">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
+      <main className="main">
+        <h1 className="title">
           Learn <a href="https://nextjs.org">Next.js!</a>
         </h1>
       </main>
+
+      <HomeStyles admin={false} />
+      <GlobalStyles />
     </div>
-  )
+  );
 }
+
+const HomeStyles = ({ admin }) => {
+  return (
+    <style jsx>{`
+      .container {
+        min-height: 100vh;
+        padding: 0 0.5rem;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        background: ${admin ? "green" : "none"};
+      }
+
+      .main {
+        padding: 5rem 0;
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+      }
+
+      .title a {
+        color: #0070f3;
+        text-decoration: none;
+      }
+
+      .title a:hover,
+      .title a:focus,
+      .title a:active {
+        text-decoration: underline;
+      }
+
+      .title {
+        margin: 0;
+        line-height: 1.15;
+        font-size: 4rem;
+      }
+
+      .title {
+        text-align: center;
+      }
+    `}</style>
+  );
+};
+
+const GlobalStyles = () => {
+  return (
+    <style jsx global>
+      {`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+
+        a {
+          color: inherit;
+          text-decoration: none;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}
+    </style>
+  );
+};
