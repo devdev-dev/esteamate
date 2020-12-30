9ff37762f0b9b76c5d558460a23490c046e10254
Hendrik Schuster <h.schuster@deviantdev.com>
2020-12-30 23:08:23 +0100

    Revert styles separation as it does not work as intended with styled jsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,85 +7,52 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main className="main">
         <h1 className="title">
           Getting started with <a href="https://nextjs.org">Next.js!</a>
         </h1>
       </main>
-
-      <HomeStyles admin={false} />
-      <GlobalStyles />
-    </div>
-  );
-}
-
-const HomeStyles = ({ admin }) => {
-  return (
-    <style jsx>{`
-      .container {
-        min-height: 100vh;
-        padding: 0 0.5rem;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        background: ${admin ? 'green' : 'none'};
-      }
-
-      .main {
-        padding: 5rem 0;
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      .title a {
-        color: #0070f3;
-        text-decoration: none;
-      }
-
-      .title a:hover,
-      .title a:focus,
-      .title a:active {
-        text-decoration: underline;
-      }
-
-      .title {
-        margin: 0;
-        line-height: 1.15;
-        font-size: 4rem;
-      }
-
-      .title {
-        text-align: center;
-      }
-    `}</style>
-  );
-};
-
-const GlobalStyles = () => {
-  return (
-    <style jsx global>
-      {`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+      <style jsx>{`
+        .container {
+          min-height: 100vh;
+          padding: 0 0.5rem;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
         }
 
-        a {
-          color: inherit;
+        .main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        .title a {
+          color: #0070f3;
           text-decoration: none;
         }
 
-        * {
-          box-sizing: border-box;
+        .title a:hover,
+        .title a:focus,
+        .title a:active {
+          text-decoration: underline;
         }
-      `}
-    </style>
+
+        .title {
+          margin: 0;
+          line-height: 1.15;
+          font-size: 4rem;
+        }
+
+        .title {
+          text-align: center;
+        }
+      `}</style>
+      );
+    </div>
   );
-};
+}
